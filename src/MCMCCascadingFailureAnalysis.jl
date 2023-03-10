f38d1cfaaeac4ce61c87e91b282bd9ca32cd83a5
Ferdinand <ferdinand.most@web.de>
2023-03-10 09:52:52 +0100

    added the relevant package.jl file in src folder

---
+++ src/MCMCCascadingFailureAnalysis.jl
@@ -0,0 +1,11 @@
+module MCMCCascadingFailureAnalysis
+
+####################### import external packages ###############################
+using LinearAlgebra
+
+############################ source files ######################################
+include("simulation_relevant_functions.jl")
+
+########################### export functions ###################################
+# simulated annealing functions
+export flow
